3848b033c0124309c80d8de444155f5a856a460f
LAPTOP-EURV9OG3\neerg <catwal@ceh.ac.uk>
2026-02-14 08:00:17 +0000

    wrote the code for the questions

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import InquirerPy
 
 # Questions for the README file
+# Project Title
+# Description
+# Installation Instructions
+# Usage Information
+# License (choose from a dropdown list)
+# Author Name
+# Contact Information
 
-
+questions = [
+    {"type": "input", "name": "project-title", "message": "What is the title of your project?"},
+    {"type": "input", "name": "description", "message": "Write a decsription of your project"},
+    {"type": "input", "name": "installation-instructions", "message": "Give the installation instructions"},
+    {"type": "input", "name": "usage-info", "message": "Provide come usage information"},
+    {"type": "list", "name": "license", "message": "Select the license", "choices": [
+        "MIT License", "Apache License 2.0", "GNU GPL v3", "GNU LGPL v3", 
+        "Mozilla Public License 2.0", "Creative Commons", "Unlicensed"]},
+    {"type": "input", "name": "author-name", "message": "What is the name of the author?"},
+    {"type": "input", "name": "contact-info", "message": "Provide some contact imformation"}
+]
 
